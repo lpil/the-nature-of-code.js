dabbd4e984ac41c828a1c45cf2ab35c068d2e715
Louis Pilfold <louis@lpil.uk>
2015-05-24 22:39:49 +0100

    Some page styling

--- _includes/chapter-one/03.js
+++ _includes/chapter-one/03.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   var mouse = createVector(mouseX, mouseY);
   background(255);
-  mouse.sub(center);            // mouse from the center, not top-left
-  translate(width/2, height/2); // move 0,0 to center of window
+  mouse.sub(center);             // mouse from the center, not top-left
+  translate(center.x, center.y); // move 0,0 to center of window
   line(0, 0, mouse.x, mouse.y);
 }
