508e2fcaeb17097ed04b9c8d2aef05c5e16d6d03
Louis Pilfold <louis@lpil.uk>
2015-05-24 22:23:20 +0100

    Subtracting vectors

---
+++ _includes/chapter-one/03.js
@@ -0,0 +1,14 @@
+var center;
+
+function setup() {
+  createCanvas(500, 350).parent('canvas-container');
+  center = createVector(width/2, height/2);
+}
+
+function draw() {
+  var mouse = createVector(mouseX, mouseY);
+  background(255);
+  mouse.sub(center);            // mouse from the center, not top-left
+  translate(width/2, height/2); // move 0,0 to center of window
+  line(0, 0, mouse.x, mouse.y);
+}
